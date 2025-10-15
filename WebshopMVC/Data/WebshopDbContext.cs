9c74cf1c6ce62e125af7f3dd267142b920b6037d
Benedek Benkó <beni200306@gmail.com>
2025-10-15 18:16:21 +0200

    db context done

---
+++ WebshopMVC/Data/WebshopDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebshopMVC.Models;
+
+namespace WebshopMVC.Data
+{
+    public class WebshopDbContext : DbContext
+    {
+        public WebshopDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<OrderItem> OrderItems{ get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Webshop;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
