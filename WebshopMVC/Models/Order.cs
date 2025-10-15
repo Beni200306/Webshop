904e08c898671feaca48ea1ce17c428b64c9e314
Benedek Benkó <beni200306@gmail.com>
2025-10-15 18:09:38 +0200

    model classes done

--- WebshopMVC/Models/Order.cs
+++ WebshopMVC/Models/Order.cs
@@ -1,6 +1,21 @@
-﻿namespace WebshopMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebshopMVC.Models
 {
     public class Order
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string CustomerName{ get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public ICollection<OrderItem> Items { get; set; }
+        public Order()
+        {
+            Items = new HashSet<OrderItem>();
+        }
     }
 }
--- WebshopMVC/Models/OrderItem.cs
+++ WebshopMVC/Models/OrderItem.cs
@@ -1,6 +1,19 @@
-﻿namespace WebshopMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebshopMVC.Models
 {
     public class OrderItem
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public int Quantity { get; set; }
     }
 }
