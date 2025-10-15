9dad1f624f3488b9254593e097278a0ba136b57c
Benedek Benkó <beni200306@gmail.com>
2025-10-15 17:50:36 +0200

    CartItem class done

--- WebshopMVC/Models/CartItem.cs
+++ WebshopMVC/Models/CartItem.cs
@@ -1,6 +1,15 @@
-﻿namespace WebshopMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebshopMVC.Models
 {
     public class CartItem
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } 
+        public int ProductId { get; set; }
+        public virtual Product Product { get; set; }
+        public int Quantity { get; set; }
     }
 }
